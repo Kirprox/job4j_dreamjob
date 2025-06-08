f800aac2f2f0fca022f2ccb2a77437e3edd49115
Kir <kirpro95@yandex.ru>
2025-06-08 22:40:55 +0300

    4. Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -0,0 +1,76 @@
+package ru.job4j.dreamjob.repository;
+
+import ru.job4j.dreamjob.model.Vacancy;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryVacancyRepository implements VacancyRepository {
+
+    private static final MemoryVacancyRepository INSTANCE = new MemoryVacancyRepository();
+
+    private int nextId = 1;
+
+    private final Map<Integer, Vacancy> vacancies = new HashMap<>();
+
+    private MemoryVacancyRepository() {
+        save(new Vacancy(0, "Intern Java Developer",
+                "Стажер, начинающий разработчик",
+                LocalDateTime.now()));
+        save(new Vacancy(0, "Junior Java Developer",
+                "Младший разработчик с базовыми знаниями Java",
+                LocalDateTime.now()));
+        save(new Vacancy(0, "Junior+ Java Developer",
+                "Младший разработчик с коммерческим опытом "
+                       + "и уверенным знанием Java",
+                LocalDateTime.now()));
+        save(new Vacancy(0, "Middle Java Developer",
+                "Разработчик среднего уровня с опытом "
+                        + "участия в проектах",
+                LocalDateTime.now()));
+        save(new Vacancy(0, "Middle+ Java Developer",
+                "Разработчик среднего уровня с лидерскими качествами "
+                        + "и опытом менторства",
+                LocalDateTime.now()));
+        save(new Vacancy(0, "Senior Java Developer",
+                "Опытный разработчик, способный принимать "
+                       + "архитектурные решения",
+                LocalDateTime.now()));
+    }
+
+    public static MemoryVacancyRepository getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Vacancy save(Vacancy vacancy) {
+        vacancy.setId(nextId++);
+        vacancies.put(vacancy.getId(), vacancy);
+        return vacancy;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        vacancies.remove(id);
+    }
+
+    @Override
+    public boolean update(Vacancy vacancy) {
+        return vacancies.computeIfPresent(vacancy.getId(),
+                (id, oldVacancy) -> new Vacancy(oldVacancy.getId(), vacancy.getTitle(),
+                        vacancy.getDescription(), vacancy.getCreationDate())) != null;
+    }
+
+    @Override
+    public Optional<Vacancy> findById(int id) {
+        return Optional.ofNullable(vacancies.get(id));
+    }
+
+    @Override
+    public Collection<Vacancy> findAll() {
+        return vacancies.values();
+    }
+}
