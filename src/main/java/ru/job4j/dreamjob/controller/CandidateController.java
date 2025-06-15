b41be3036c4e2c31b5668e18b2d31cdfe40de691
kir <kirpro95@yandex.ru>
2025-06-15 12:27:51 +0300

    1. Слоеная архитектура. [#504851]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -1,22 +1,20 @@
 package ru.job4j.dreamjob.controller;
 
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.dreamjob.model.Candidate;
-import ru.job4j.dreamjob.repository.CandidateRepository;
-import ru.job4j.dreamjob.repository.MemoryCandidateRepository;
+import ru.job4j.dreamjob.service.SimpleCandidateService;
 
 @Controller
 @RequestMapping("/candidates")
 public class CandidateController {
-    private final CandidateRepository candidateRepository =
-            MemoryCandidateRepository.getInstance();
+    private final SimpleCandidateService simpleCandidateService =
+            SimpleCandidateService.getInstance();
 
     @GetMapping
     public String getAll(Model model) {
-        model.addAttribute("candidates", candidateRepository.findAll());
+        model.addAttribute("candidates", simpleCandidateService.findAll());
         return "candidates/list";
     }
 
@@ -27,13 +25,13 @@ public class CandidateController {
 
     @PostMapping("/create")
     public String create(@ModelAttribute Candidate candidate) {
-        candidateRepository.save(candidate);
+        simpleCandidateService.save(candidate);
         return "redirect:/candidates";
     }
 
     @GetMapping("/{id}")
     public String getById(Model model, @PathVariable int id) {
-        var candidateOptional = candidateRepository.findById(id);
+        var candidateOptional = simpleCandidateService.findById(id);
         if (candidateOptional.isEmpty()) {
             model.addAttribute("message",
                     "Кандидат с указанным Id не найден");
@@ -45,7 +43,7 @@ public class CandidateController {
 
     @PostMapping("/update")
     public String update(@ModelAttribute Candidate candidate, Model model) {
-        var isUpdate = candidateRepository.update(candidate);
+        var isUpdate = simpleCandidateService.update(candidate);
         if (!isUpdate) {
             model.addAttribute("message",
                     "Кандидат с указанным Id не найден");
@@ -55,7 +53,7 @@ public class CandidateController {
 
     @GetMapping("/delete/{id}")
     public String delete(Model model, @PathVariable int id) {
-        var isDeleted = candidateRepository.deleteById(id);
+        var isDeleted = simpleCandidateService.deleteById(id);
         if (!isDeleted) {
             model.addAttribute("message",
                     "Кандидат с указанным Id не найден");
